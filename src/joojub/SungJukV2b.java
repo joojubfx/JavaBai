e6bf80e1795dc393813a153b401034bda6033141
joojubfx <joojubfx@hotmail.com>
2020-11-17 15:43:26 +0900

    선생님 작성 v2b

--- src/joojub/SungJukV2b.java
+++ src/joojub/SungJukV2b.java
@@ -13,8 +13,11 @@ import java.util.Scanner;
  * name, kor, eng, mat, sum, mean, grd
  * 학점기준은 수우미양가로 하되 switch 문으로
  * 만점은 100점으로.
+ *
+ * 성적처리 결과 출력시 출력문을 하나만 사용하기로 함
+ * 그리고 Math.round()를 이용해서 평균은 소수점 둘째자리까지 끊어서 출력
  */
-public class SungJukV2 {
+public class SungJukV2b {
 //    public class Student {
 //        String name;
 //        int kor, eng, mat, sum, mean, grd;
@@ -36,13 +39,15 @@ public class SungJukV2 {
     public static void main(String[] args) {
         //변수 선언
         String name = "";
-        int kor = 0;
-        int eng = 0;
-        int mat = 0;
-        int sum = 0;
-        double mean = 0.0;
-        char grd = '가';
+        int kor;
+        int eng;
+        int mat;
+        int sum;
+        double mean;
+        char grd;
+        String fmt = " 이름 : %s\n 국어 : %d\n 영어 : %d\n 수학 : %d\n 총점 : %d\n 평균 : %.2f\n 학점 : %c\n";
 
+        String result;
         //처리
 
         //성적데이터를 입력 하기 위해 스캐너 클래스 초기화
@@ -75,19 +80,15 @@ public class SungJukV2 {
             case 6 : grd = '양'; break;
             default : grd = '가';
         }
-        //String.format(형식지정자, 변수들)
-        mean = Double.parseDouble(String.format("%.1f", mean));
+        //Math.round(값) : 반올림
+        mean = Math.round(mean*100)/100.0;
+
+        result = String.format(fmt, name, kor, eng, mat, sum, mean, grd);
 
         //결과출력
 
-        System.out.println("이름 : " + name);
-        System.out.println("국어 : " + kor);
-        System.out.println("영어 : " + eng);
-        System.out.println("수학 : " + mat);
-        System.out.println("----------");
-        System.out.println("총점 : " + sum);
-        System.out.println("평균 : " + mean);
-        //System.out.printf("평균 : %.1f\n", mean);
-        System.out.println("학점 : " + grd);
+        // 문자열 연결 연산자로 문장을 만드는 경우 String 자료형의 특성때문에 성능저하 발생
+
+        System.out.println(result);
     }
 }
