826302f6db3b8a423ce3170291285bf31a9946cd
joojubfx <joojubfx@hotmail.com>
2020-11-17 11:29:40 +0900

    내가 작성한 성적 v1

--- src/joojub/SungJukV1.java
+++ src/joojub/SungJukV1.java
@@ -12,22 +12,45 @@ package joojub;
  * 만점은 100점으로.
  */
 public class SungJukV1 {
-    public class Student {
-        String name;
-        int kor, eng, mat, sum, mean, grd;
-    }
+//    public class Student {
+//        String name;
+//        int kor, eng, mat, sum, mean, grd;
+//    }
+//    public static void main(String[] args) {
+//        Student std1 = new Student();
+//        std1.name = "닐리리";
+//        std1.eng = 80;
+//        std1.kor = 70;
+//        std1.mat = 90;
+//
+//        std1.sum = std1.eng+std1.kor+std1.mat;
+//        std1.mean = std1.sum/3;
+//
+//        std1.grd = (std1.mean >=90) ? (std1.mean>=70 && std1.mean<=89) ? (std1.mean  )
+//
+//        }
+//    }
     public static void main(String[] args) {
-        Student std1 = new Student();
-        std1.name = "닐리리";
-        std1.eng = 80;
-        std1.kor = 70;
-        std1.mat = 90;
+        //변수 선언
+        String name = "지현";
+        int kor = 99;
+        int eng = 98;
+        int mat = 99;
+        int sum = 99;
+        double mean = 0.0;
+        char grd = '가';
 
-        std1.sum = std1.eng+std1.kor+std1.mat;
-        std1.mean = std1.sum/3;
+        //처리
+        sum = kor + eng + mat;
+        mean = sum / 3;
 
-        std1.grd = (std1.eng >=90) ? (std1.eng>=70 && std1.eng<=89) ? (std1.eng )
-
-        }
+        //결과출력
+        System.out.println("이름 : " + name);
+        System.out.println("국어 :" + kor);
+        System.out.println("영어 :" + eng);
+        System.out.println("수학 :" + mat);
+        System.out.println("----------");
+        System.out.println("총점 :" + sum);
+        System.out.println("평균 :" + mean);
     }
 }
